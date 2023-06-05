9b1256023cb07eb6f0cc62c267029333d70b6c5a
Tushar Sharma <tush.sharma2504@gmail.com>
2023-06-05 23:39:21 +0530

    Improved Validation

--- components/SimpleInput.js
+++ components/SimpleInput.js
@@ -1,41 +1,64 @@
-import { useState, useRef } from "react";
+import { useEffect, useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  const [enteredName, setEnteredName] = useState();
-  const [isValid, setIsValid] = useState(true);
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log('Name Input is valid!');
+    }
+  }, [enteredNameIsValid]);
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    if (enteredName.trim() === ""){
-      setIsValid(false)
-       return;
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
     }
-    setIsValid(true);
+
+    setEnteredNameIsValid(true);
+
     console.log(enteredName);
-    // const enteredValue = nameInputRef.current.value;
-    // console.log(enteredValue);
-    setEnteredName("");
-    // nameInputRef.current.value = ''; //  As it is directly manipulation DOM, It is not ideal.
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
+    setEnteredName('');
   };
-  const inputClass = isValid? 'form-control' : 'form-control invalid';
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className={inputClass}>
-        <label htmlFor="name">Your Name</label>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
         <input
           ref={nameInputRef}
-          value={enteredName}
-          type="text"
-          id="name"
+          type='text'
+          id='name'
           onChange={nameInputChangeHandler}
+          value={enteredName}
         />
-        {!isValid && <p className="error-text">Name cannot be empty!</p>}
+        {nameInputIsInvalid && (
+          <p className='error-text'>Name must not be empty.</p>
+        )}
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
